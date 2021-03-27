e0e0b5afade4c7edcddc4bfc02093ce52459dd01
yunus sasa <sasayunus03@gmail.com>
2021-03-27 14:01:37 +0300

    27.03.2021

---
+++ src/app/models/car.ts
@@ -0,0 +1,12 @@
+export interface Car
+{
+    id:number;
+    brandId:number;
+    colorId:number;
+    brandName:string;
+    colorName:string;
+    dailyPrice:number;
+    description:string;
+    modelYear:number;
+
+}
---
+++ src/app/models/rental.ts
@@ -0,0 +1,8 @@
+export interface Rental
+{
+    id:number;
+    brandName:string;
+    customerName:string;
+    rentDate:Date;
+    returnDate:Date;
+}
